21ac2440becb6d4b12e998bbfe6c0f21554defbc
JayPatel <jay.patel1@gameskraft.com>
2024-03-13 15:21:38 +0530

    mobile view setup started

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import TitleStripe from '../../components/TitleStripe/TitleStripe';
 
 function Content(){
@@ -6,18 +5,18 @@ function Content(){
     return (
         <div className="w-full">
             <div className='w-full'>
-                <div id="home" className="w-full min-h-[calc(100vh-48px)] h-auto relative content-block">
+                <div id="home" className="w-full min-h-[100vh] md:min-h-[calc(100vh-48px)] h-auto relative content-block">
                 </div>
-                <div id="work" className="w-full min-h-[calc(100vh-48px)] h-auto relative content-block">
+                <div id="work" className="w-full min-h-[100vh] md:min-h-[calc(100vh-48px)] h-auto relative content-block">
                     <TitleStripe title={'Work Experience'}></TitleStripe>
                 </div>
-                <div id="education" className="w-full min-h-[calc(100vh-48px)] h-auto relative content-block">
+                <div id="education" className="w-full min-h-[100vh] md:min-h-[calc(100vh-48px)] h-auto relative content-block">
                     <TitleStripe title={'Education'}></TitleStripe>
                 </div>
-                <div id="projects" className="w-full min-h-[calc(100vh-48px)] h-auto relative content-block">
+                <div id="projects" className="w-full min-h-[100vh] md:min-h-[calc(100vh-48px)] h-auto relative content-block">
                     <TitleStripe title={'Projects'}></TitleStripe>
                 </div>
-                <div id="contact" className="w-full min-h-[calc(100vh-48px)] h-auto relative content-block">
+                <div id="contact" className="w-full min-h-[100vh] md:min-h-[calc(100vh-48px)] h-auto relative content-block">
                     <TitleStripe title={'Contact'}></TitleStripe>
                 </div>
             </div>
